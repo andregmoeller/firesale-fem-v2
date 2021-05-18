6f59b0cd96c37147323bb8b0e5bf28ba7d826ec9
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2021-05-18 23:41:48 +0200

    Opens files

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-const {app, BrowserWindow} = require('electron');
+const fs = require('fs');
+const { app, BrowserWindow, dialog } = require('electron');
 
 let mainWindow = null;
 
@@ -11,3 +12,25 @@ app.on('ready', () => {
     mainWindow.show();
   });
 });
+
+exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+    buttonLabel: 'Unveil',
+    title: 'Open Fire Sale Document',
+    filters: [
+      { name: 'Markdown Files', extensions: ['md'] },
+      { name: 'Text Files', extensions: ['txt', 'text'] }
+    ],
+  });
+
+  if (!files) return;
+
+  const file = files[0];
+  openFile(file);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  mainWindow.webContents.send('file-opened', file, content);
+};
